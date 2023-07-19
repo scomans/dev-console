0e89eda87cec1148c95d095c79a601f1bfa74377
Sebastian Comans <sebastian.comans@i40mc.de>
2023-07-19 12:27:07 +0200

    :bug: prevent some common browser hotkeys

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -6,6 +6,43 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
+  document.addEventListener(
+    'contextmenu',
+    function (event) {
+      event.preventDefault();
+      return false;
+    },
+    { capture: true },
+  );
+
+  document.onkeydown = function (e) {
+    if (e.ctrlKey && e.shiftKey) {
+      switch (e.code.toLowerCase()) {
+        case 'keyi':
+        case 'keyj':
+        case 'keyc':
+          console.log('PREVENT');
+          return false;
+      }
+    } else if (e.ctrlKey) {
+      switch (e.code.toLowerCase()) {
+        case 'semicolon':
+        case 'keyr':
+        case 'keyu':
+        case 'keyp':
+        case 'f5':
+          return false;
+      }
+    } else {
+      switch (e.code.toLowerCase()) {
+        case 'f5':
+        case 'f7':
+        case 'f12':
+          return false;
+      }
+    }
+  };
 }
 
 platformBrowserDynamic()
