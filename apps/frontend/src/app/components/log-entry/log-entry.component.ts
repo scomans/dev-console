4b187edafe5b27b81ac611d57b3121082ae066dc
scomans <scomans@users.noreply.github.com>
2023-12-10 15:13:38 +0100

    👌 IMPROVE: use signals for the log entry data

--- apps/frontend/src/app/components/log-entry/log-entry.component.ts
+++ apps/frontend/src/app/components/log-entry/log-entry.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, Input, Signal, signal } from '@angular/core';
 import { LogEntryWithSource } from '@dev-console/types';
+import { NgIf } from '@angular/common';
+import { isNil } from 'lodash-es';
 
 
 export type LogEntryWithSourceAndColor = LogEntryWithSource & { color: string }
@@ -10,30 +12,29 @@ export type LogEntryWithSourceAndColor = LogEntryWithSource & { color: string }
   styleUrls: ['./log-entry.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [
+    NgIf,
+  ],
 })
 export class LogEntryComponent {
-
-  borderColor: string;
-  backgroundColor: string;
-
-  private _entry: LogEntryWithSourceAndColor;
-
-  get entry(): LogEntryWithSourceAndColor {
-    return this._entry;
-  }
+  borderColor: Signal<string>;
+  backgroundColor: Signal<string>;
+  logEntry = signal<LogEntryWithSourceAndColor | undefined>(undefined);
 
   @Input() set entry(value: LogEntryWithSourceAndColor) {
-    this._entry = value;
-    this.updateColors();
+    this.logEntry.set(value);
   }
 
-  updateColors() {
-    if (this._entry) {
-      this.borderColor = this._entry.color;
-      this.backgroundColor = this._entry.type === 'data' ?
-        this._entry.color + '14' :
-        (this._entry.type === 'info' ? 'rgba(32, 178, 170, 0.1)' : 'rgba(196, 2, 2, 0.1)');
-    }
+  constructor() {
+    this.borderColor = computed(() => this.logEntry().color || 'transparent');
+    this.backgroundColor = computed(() => {
+      const entry = this.logEntry();
+      if (isNil(entry)) {
+        return 'transparent';
+      }
+      return entry.type === 'data' ?
+        entry.color + '14' :
+        (entry.type === 'info' ? 'rgba(32, 178, 170, 0.1)' : 'rgba(196, 2, 2, 0.1)');
+    });
   }
-
 }
